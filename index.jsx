c6b96ea273f47a3aba723362969765efbc5471ad
testai223 <test_ai_w_24433@proton.me>
2025-06-26 00:28:22 +0200

    Make demo runnable via simple HTML

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import React from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+import LoadFlowCalculator from "./LoadFlowCalculator.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  React.createElement(LoadFlowCalculator)
+);
